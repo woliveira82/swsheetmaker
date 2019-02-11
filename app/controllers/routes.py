cb2d40a3d7571b2f0698585d998ec5bc6699f4ca
Weiner <woliveira82@gmail.com>
2019-02-11 21:48:31 -0200

    New routes

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -7,6 +7,11 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/')
-def charsheet():
-    return render_template('charsheet.html')
+@app.route('/charsheet/<int:id>', methods = ['GET'])
+def charsheet(id):
+    return render_template('charsheet.html', id = id)
+
+
+@app.route('/config')
+def config():
+    return render_template('config.html')
