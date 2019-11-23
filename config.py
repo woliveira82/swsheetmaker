dcf5f58ee21c7bbb987e5b6bc404fa4e061bd48a
Weiner <woliveira82@gmail.com>
2019-11-23 14:08:53 -0300

    First migration and db path config

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+import os.path
+
+def sqlite_path():
+    sqlite_path = os.path.dirname(os.path.abspath(__file__))
+    return sqlite_path
+
 ENV = 'production'
 DEBUG = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}storage.db'.format(sqlite_path())
 SQLALCHEMY_TRACK_MODIFICATIONS = False
