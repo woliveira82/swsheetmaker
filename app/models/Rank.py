eff2fd2ab9d60cff2a11750d80a9de0838990a26
Weiner <woliveira82@gmail.com>
2020-01-16 22:03:14 -0300

    Creating Rank model

--- app/models/Rank.py
+++ app/models/Rank.py
@@ -1,16 +1,55 @@
 from app import db
+from app.inc import Dao
 
 
-class Rank(db.Model):
+class Rank(db.Model, Dao):
     
     __tablename__ = 'rank'
     
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(120), unique = True, nullable = False)
-    xp = db.Column(db.Integer, unique = True)
+    __id = db.Column(db.Integer, primary_key=True)
+    __name = db.Column(db.String(16), unique=True, nullable=False)
+    __xp = db.Column(db.Integer, unique=True, nullable=False)
 
 
-    def __init__(self, name, xp):
-        self.id = id
-        self.name = name
-        self.xp = xp
+    def __init__(self, name, xp, id=None):
+        self.__id = id
+        self.__name = name
+        self.__xp = xp
+
+    
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'xp': self.xp,
+        }
+
+
+    @property
+    def id(self):
+        return self.__id
+
+    
+    @id.setter
+    def id(self, id):
+        self.__id = id
+
+
+    @property
+    def name(self):
+        return self.__name
+
+    
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+
+    @property
+    def xp(self):
+        return self.__xp
+
+    
+    @xp.setter
+    def xp(self, xp):
+        self.__xp = xp
